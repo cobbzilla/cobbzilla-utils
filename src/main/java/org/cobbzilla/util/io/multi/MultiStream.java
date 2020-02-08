f9d652ba000b3225428cb4ce7dca9f7aa4fb1650
Jonathan Cobb <jonathan@kyuss.org>
2020-02-07 19:20:00 -0500

    add trace logging

--- src/main/java/org/cobbzilla/util/io/multi/MultiStream.java
+++ src/main/java/org/cobbzilla/util/io/multi/MultiStream.java
@@ -24,31 +24,44 @@ public class MultiStream extends InputStream {
         currentStream = r;
     }
 
+    public int pendingStreamCount () { return streams.size() - streamIndex; }
+
     public MultiStream (InputStream r) { this(r, false); }
+
+    @Override public String toString () {
+        return "MultiStream{"+streams.size()+" streams, index="+streamIndex+", EOS="+endOfStreams+"}";
+    }
 
     public void addStream (InputStream in) {
         if (endOfStreams) {
             log.warn("addStream: endOfStreams is true, not adding InputStream");
         } else {
             streams.add(in);
+            if (log.isTraceEnabled()) log.trace("addStream: added stream ("+in.getClass().getSimpleName()+"). this="+this);
         }
     }
 
     public void addLastStream (InputStream in) {
         addStream(in);
         endOfStreams = true;
+        if (log.isTraceEnabled()) log.trace("addLastStream: added last stream ("+in.getClass().getSimpleName()+"). this="+this);
     }
 
     @Override public int read() throws IOException {
         final int val = currentStream.read();
         if (val == -1) {
             if (streamIndex == streams.size()-1) {
+                if (log.isTraceEnabled()) log.trace("read(byte): end of all streams? this="+this);
                 return endOfStreams ? -1 : 0;
             }
             currentStream.close();
             streamIndex++;
             currentStream = streams.get(streamIndex);
+            if (log.isTraceEnabled()) log.trace("read(byte): end of all stream, advanced to next stream ("+currentStream.getClass().getSimpleName()+"). this="+this);
             return read();
+
+        } else {
+            if (log.isTraceEnabled()) log.trace("read(byte): one byte read. this="+this);
         }
         return val;
     }
@@ -57,17 +70,23 @@ public class MultiStream extends InputStream {
         final int count = currentStream.read(buf, off, len);
         if (count == -1) {
             if (streamIndex == streams.size()-1) {
+                if (log.isTraceEnabled()) log.trace("read(byte[]): end of all streams? this="+this);
                 return endOfStreams ? -1 : 0;
             }
             currentStream.close();
             streamIndex++;
             currentStream = streams.get(streamIndex);
+            if (log.isTraceEnabled()) log.trace("read(byte[]): end of all stream, advanced to next stream ("+currentStream.getClass().getSimpleName()+"). this="+this);
             return read(buf, off, len);
+
+        } else {
+            if (log.isTraceEnabled()) log.trace("read(byte[]): "+count+" bytes read. this="+this);
         }
         return count;
     }
 
     @Override public void close() throws IOException {
+        if (log.isTraceEnabled()) log.trace("close: closing current stream ("+(currentStream == null ? "null" : currentStream.getClass().getSimpleName())+"). this="+this);
         if (currentStream != null) currentStream.close();
     }
 
