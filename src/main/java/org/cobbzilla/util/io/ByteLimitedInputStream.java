825ce0905f5fc562a796eeca3ce91cd250016999
Jonathan Cobb <jonathan@kyuss.org>
2020-03-31 23:47:55 -0400

    ByteLimitedInputStream now throws IOException if stream limit exceeded

--- src/main/java/org/cobbzilla/util/io/ByteLimitedInputStream.java
+++ src/main/java/org/cobbzilla/util/io/ByteLimitedInputStream.java
@@ -18,6 +18,7 @@ public class ByteLimitedInputStream extends InputStream {
 
     @Getter private long count = 0;
     @Getter private long limit;
+    @Getter private boolean eos = false;
 
     public double getPercentDone () { return ((double) count) / ((double) limit); }
 
@@ -27,24 +28,40 @@ public class ByteLimitedInputStream extends InputStream {
     }
 
     @Override public int read(byte[] b) throws IOException {
-        if (count >= limit) return -1;
+        if (eosOrLimitExceeded() == -1) return -1;
         final int read = delegate.read(b);
-        if (read != -1) count += read;
+        incr(read);
         return read;
     }
 
     @Override public int read(byte[] b, int off, int len) throws IOException {
-        if (count >= limit) return -1;
+        if (eosOrLimitExceeded() == -1) return -1;
         final int read = delegate.read(b, off, len);
-        if (read != -1) count += read;
+        incr(read);
         return read;
     }
 
     @Override public int read() throws IOException {
-        if (count >= limit) return -1;
+        if (eosOrLimitExceeded() == -1) return -1;
         final int read = delegate.read();
-        if (read != -1) count++;
+        incr(read);
         return read;
+    }
+
+    public int eosOrLimitExceeded() throws IOException{
+        if (count >= limit) {
+            if (!eos) throw new IOException("cannot continue reading: stream is larger than " + limit + " bytes");
+            return -1;
+        }
+        return 0;
+    }
+
+    public void incr(int read) {
+        if (read == -1) {
+            eos = true;
+        } else {
+            count += read;
+        }
     }
 
 }
