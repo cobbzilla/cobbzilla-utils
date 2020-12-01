ca6eb62941b7909c06f886974fb438db1cc35a66
Jonathan Cobb <jonathan@kyuss.org>
2020-12-01 06:48:45 -0500

    add text versions of status codes

--- src/main/java/org/cobbzilla/util/http/HttpStatusCodes.java
+++ src/main/java/org/cobbzilla/util/http/HttpStatusCodes.java
@@ -19,4 +19,21 @@ public class HttpStatusCodes {
     public static final int SERVER_UNAVAILABLE = 503;
     public static final int GATEWAY_TIMEOUT = 504;
 
+    public static final String SC_UNPROCESSABLE_ENTITY = "422";
+    public static final String SC_OK = "200";
+    public static final String SC_CREATED = "201";
+    public static final String SC_ACCEPTED = "202";
+    public static final String SC_NON_AUTHORITATIVE_INFO = "203";
+    public static final String SC_NO_CONTENT = "204";
+    public static final String SC_FOUND = "302";
+    public static final String SC_UNAUTHORIZED = "401";
+    public static final String SC_FORBIDDEN = "403";
+    public static final String SC_NOT_FOUND = "404";
+    public static final String SC_PRECONDITION_FAILED = "412";
+    public static final String SC_UNSUPPORTED_MEDIA_TYPE = "415";
+    public static final String SC_TOO_MANY_REQUESTS = "429";
+    public static final String SC_SERVER_ERROR = "500";
+    public static final String SC_SERVER_UNAVAILABLE = "503";
+    public static final String SC_GATEWAY_TIMEOUT = "504";
+
 }
